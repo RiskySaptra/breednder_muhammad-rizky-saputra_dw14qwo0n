d1714924b5342f333a2f94cc4170cd1d31908163
RiskySaptra <kiki014045@gmail.com>
2020-02-12 15:31:40 +0700

    2.Login

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, {Component,Fragment,useState} from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Button,Container,Row,Col} from 'react-bootstrap';
+import { Button,Container,Row,Col,Modal,Form} from 'react-bootstrap';
 
 
 function App() {
+  const [Login, setLogin] = useState(false);
   return (
     <div>
       <div>
       <Container>
         <Row className='mt-5'>
           <Col><h2 className="float-left">Breednder</h2></Col>
-          <Col xs='2'><Button className='btn-secondary rounded-pill'  block>Login</Button></Col>
+          <Col xs='2'><Button onClick={() => setLogin(true)} className='btn-secondary rounded-pill'  block>Login</Button></Col>
           <Col xs='2'><Button className='btn-secondary rounded-pill'  block>Register</Button></Col>
         </Row>
         <Row>
@@ -24,6 +25,29 @@ function App() {
           <Col xs='3'></Col>
         </Row>
       </Container>
+      <>
+      <Modal size="sm" show={Login} onHide={() => setLogin(false)} centered>
+        <Modal.Header closeButton>
+          <Modal.Title id="modal-sizes-title-sm">
+            Login
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button className='rounded-pill' variant="secondary" type="submit" size="sm" block>
+            Login
+          </Button>
+        </Form>
+        </Modal.Body>
+      </Modal>
+      </>
       </div>
     </div>
   );
