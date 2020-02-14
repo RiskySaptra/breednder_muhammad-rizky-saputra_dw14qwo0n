8ed0ef78905d708d2629f927673bf5c6b0ae0a85
RiskySaptra <kiki014045@gmail.com>
2020-02-14 13:16:38 +0700

    3.Register(fixed)

---
+++ src/components/LoginModal.js
@@ -0,0 +1,43 @@
+import React, {Fragment,useState} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { Button,Modal,Form} from 'react-bootstrap';
+import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
+
+export default function LoginModal() {
+  const [Login, setLogin] = useState(false);
+    return (
+      <Fragment>
+        <Button onClick={() => setLogin(true)} className='btn-secondary rounded-pill'  block>Login</Button>
+        <Modal size="sm" show={Login} onHide={() => setLogin(false)} centered>
+          <Modal.Header closeButton>
+            <Modal.Title id="modal-sizes-title-sm">
+              Login
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <Form>
+            <Form.Group controlId="Email">
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="Password">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="RememberMe">
+              <Form.Check type="checkbox" label="Remember Me" />
+            </Form.Group>
+          </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Link to="/index">
+              <Button className='rounded-pill' variant="secondary" type="submit" size="sm">
+                Login
+              </Button>
+            </Link>
+          </Modal.Footer>
+        </Modal>
+      </Fragment>
+    ); 
+}
---
+++ src/page/Index.js
@@ -0,0 +1,50 @@
+import React, {Component,Fragment,useState} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Landing.css';
+
+import Deck from "../components/Deck";
+import data from "../data.js";
+import {Nav,Navbar,NavDropdown,Form,FormControl, CardGroup} from 'react-bootstrap'
+
+import { Image,Card,Button,CardDeck } from 'react-bootstrap';
+
+class  Index extends Component {
+    render(){
+      return (
+        <div className='container'>
+            <div className='boxLeft'>
+            <Navbar bg="dark" variant="dark" className='sticky-top'>
+              <Navbar.Brand href="#home">
+                <img
+                  alt=""
+                  src="/logo192.png"
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                />{' '}
+                Afdool
+              </Navbar.Brand>
+            </Navbar>
+            <div className='text-center'>
+              {data.map(item => (
+                <img className='box-image' src={item.pics} />
+              ))}
+            </div>
+            </div>
+            <div id='s' className='boxRight test'>
+              <Deck/>
+            </div>
+            <div className='boxFooter'>
+              <div className='text-center'>
+                <button>berak</button><button>berak</button><button>berak</button>
+              </div>
+            </div>
+        </div>
+      );
+    }
+
+  }
+
+
+  export default Index;
